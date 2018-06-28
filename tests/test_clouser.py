a014236f7e64e60053c2e6100f0c13ce94e25878
326487162 <Welcome@661>
2018-06-28 15:55:24 -0400

    Make closure variable changeable

--- tests/test_clouser.py
+++ tests/test_clouser.py
@@ -1,18 +1,30 @@
 from unittest import TestCase
 
-def make_averager():
+
+def make_average():
     count = 0
     total = 0
-    def averager(new_value):
-        nonlocal count, total      # 数字或不可变类型做inplace(+=)操作等同 count = count + 1，会导致（缺省）生成一个新的本地变量，
-                                   # 如果该变量不是本地变量，那么需要修饰成 global 或 nonlocal，否则会编译错误(name 'count' is not defined).
-                                   # 或一个变通的方法（python2）是将它们绑定到可变变量（字典或实例）
+
+    def average():
+        return total / count
+
+    def add_new_values(new_value):
+        # 数字或不可变类型做inplace(+=)操作等同 count = count + 1，会导致（缺省）生成一个新的本地变量，
+        # 如果该变量不是本地变量，那么需要修饰成 global 或 nonlocal，否则会编译错误(name 'count' is not defined).
+        # 或一个变通的方法（python2）是将它们绑定到可变变量（字典或实例）
+        nonlocal count, total
         count += 1
         total += new_value
-        return total / count
-    return averager
 
-class TestClouser(TestCase):
-    def test_nonlocal_from_clouser(self):
-        avg = make_averager()
-        print(avg(10))
+    return add_new_values, average
+
+
+class TestClosure(TestCase):
+    def test_nonlocal_from_closure(self):
+        add, avg = make_average()
+
+        add(10)
+        assert(10 == avg())
+
+        add(20)
+        assert(15 == avg())
