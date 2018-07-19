1d0356637603a87e8f313a91560a36b6c00a154a
326487162 <Welcome@661>
2018-07-19 14:44:17 -0400

    Add offset test for closure

--- tests/test_clouser.py
+++ tests/test_clouser.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 
 
-def make_average():
+def make_average(offset):
     count = 0
     total = 0
+    o = offset
 
     def average():
         return total / count
@@ -12,19 +13,20 @@ def make_average():
         # 数字或不可变类型做inplace(+=)操作等同 count = count + 1，会导致（缺省）生成一个新的本地变量，
         # 如果该变量不是本地变量，那么需要修饰成 global 或 nonlocal，否则会编译错误(name 'count' is not defined).
         # 或一个变通的方法（python2）是将它们绑定到可变变量（字典或实例）
-        nonlocal count, total
+        nonlocal count, total, o
         count += 1
-        total += new_value
+        total += new_value - o
 
     return add_new_values, average
 
 
 class TestClosure(TestCase):
     def test_nonlocal_from_closure(self):
-        add, avg = make_average()
+        offset = 1
+        add, avg = make_average(offset)
 
         add(10)
-        assert(10 == avg())
+        assert(9 == avg())
 
         add(20)
-        assert(15 == avg())
+        assert(14 == avg())
